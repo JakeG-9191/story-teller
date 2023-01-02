5abf7b73e3d2797b7d6e376c2bf6e58647af34f1
JakeG-9191 <jacobgarlick@msn.com>
2023-01-02 15:50:22 -0700

    continued to add basic config for ts and css, added basic navigation view, added basic state callback to kick off app

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,20 +1,16 @@
-import { Grid } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 import React from 'react'
 
-const Login = () => {
+interface Props {
+    loginCallback: () => void
+}
+
+const Login = (props: Props) => {
+    const { loginCallback } = props
     return (
         <Grid container spacing={2}>
-            <Grid item xs={8}>
-                Something
-            </Grid>
-            <Grid item xs={4}>
-                Something 2
-            </Grid>
-            <Grid item xs={4}>
-                Something 3
-            </Grid>
-            <Grid item xs={8}>
-                Something 4
+            <Grid item xs={12}>
+                <Button onClick={loginCallback}>Begin Adventure</Button>
             </Grid>
         </Grid>
     )
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './Login'
+import Navigation from './Navigation'
 
 const Main = () => {
-    return <Login />
+    const [isStarted, setIsStarted] = useState(false)
+
+    return isStarted ? (
+        <Navigation logoutCallback={() => setIsStarted(false)}>
+            <p>now the story has started</p>
+        </Navigation>
+    ) : (
+        <Login loginCallback={() => setIsStarted(true)} />
+    )
 }
 
 export default Main
