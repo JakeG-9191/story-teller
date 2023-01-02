c9b3b131a9f9632f8406756885da1f4cc993eaad
JakeG-9191 <jacobgarlick@msn.com>
2023-01-02 15:15:24 -0700

    added ts support, added mui, removed some unused files, added prettier config, added new views, added views and components directory, converted js files to ts files

---
+++ src/views/Login.tsx
@@ -0,0 +1,23 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const Login = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={8}>
+                Something
+            </Grid>
+            <Grid item xs={4}>
+                Something 2
+            </Grid>
+            <Grid item xs={4}>
+                Something 3
+            </Grid>
+            <Grid item xs={8}>
+                Something 4
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Login
